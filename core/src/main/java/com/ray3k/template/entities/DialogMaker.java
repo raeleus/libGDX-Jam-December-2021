2e76df2bca1d18c82851948f3de041c43fbb9861
raeleus <raymond.ray3k@gmail.com>
2021-12-19 18:34:55 -0800

    Updated dialogs.

--- core/src/main/java/com/ray3k/template/entities/DialogMaker.java
+++ core/src/main/java/com/ray3k/template/entities/DialogMaker.java
@@ -19,7 +19,7 @@ public class DialogMaker {
             case 0:
                 if (!defeatedZebra) {
                     deltaMultiplier = 0;
-                    var pop = createTable(() -> showMessage2());
+                    var pop = createTable(() -> showMessageZebra1());
             
                     var image = new Image(skin.getDrawable("icon-zebra"));
                     image.setScaling(Scaling.none);
@@ -36,7 +36,7 @@ public class DialogMaker {
             case 1:
                 if (!defeatedGroxar) {
                     deltaMultiplier = 0;
-                    var pop = createTable(() -> showMessage4());
+                    var pop = createTable(() -> showMessageGroxar1());
             
                     var image = new Image(skin.getDrawable("icon-groxar"));
                     image.setScaling(Scaling.none);
@@ -50,11 +50,62 @@ public class DialogMaker {
                     pop.show(gameScreen.stage);
                 }
                 break;
+            case 2:
+                if (!defeatedPanda) {
+                    deltaMultiplier = 0;
+                    var pop = createTable(() -> showMessagePanda1());
+            
+                    var image = new Image(skin.getDrawable("icon-panda"));
+                    image.setScaling(Scaling.none);
+                    pop.add(image);
+            
+                    var label = new TypingLabel(
+                            "I will melt your bones and make a candle out of it.", skin);
+                    label.setWrap(true);
+                    pop.add(label).grow();
+            
+                    pop.show(gameScreen.stage);
+                }
+                break;
+            case 3:
+                if (!defeatedLyze) {
+                    deltaMultiplier = 0;
+                    var pop = createTable(() -> showMessageLyze1());
+            
+                    var image = new Image(skin.getDrawable("icon-lyze"));
+                    image.setScaling(Scaling.none);
+                    pop.add(image);
+            
+                    var label = new TypingLabel(
+                            "Duty requires that I subdue you. Take note that I derive no pleasure from taking your life.", skin);
+                    label.setWrap(true);
+                    pop.add(label).grow();
+            
+                    pop.show(gameScreen.stage);
+                }
+                break;
+            case 4:
+                if (!defeatedJohn) {
+                    deltaMultiplier = 0;
+                    var pop = createTable(() -> showMessageJohn1());
+            
+                    var image = new Image(skin.getDrawable("icon-lyze"));
+                    image.setScaling(Scaling.none);
+                    pop.add(image);
+            
+                    var label = new TypingLabel(
+                            "HEEEEEEEELLLP MEEEEEEEE...", skin);
+                    label.setWrap(true);
+                    pop.add(label).grow();
+            
+                    pop.show(gameScreen.stage);
+                }
+                break;
         }
     }
     
-    private static void showMessage2() {
-        var pop = createTable(() -> showMessage3());
+    private static void showMessageZebra1() {
+        var pop = createTable(() -> showMessageZebra2());
     
         var image = new Image(skin.getDrawable("icon-dq"));
         image.setScaling(Scaling.none);
@@ -67,7 +118,7 @@ public class DialogMaker {
         pop.show(gameScreen.stage);
     }
     
-    private static void showMessage3() {
+    private static void showMessageZebra2() {
         var pop = createTable(() -> {
             deltaMultiplier = 1;
         });
@@ -83,7 +134,7 @@ public class DialogMaker {
         pop.show(gameScreen.stage);
     }
     
-    private static void showMessage4() {
+    private static void showMessageGroxar1() {
         var pop = createTable(() -> {
             deltaMultiplier = 1;
         });
@@ -93,6 +144,54 @@ public class DialogMaker {
         pop.add(image);
         
         var label = new TypingLabel("What???", skin);
+        label.setWrap(true);
+        pop.add(label).grow();
+        
+        pop.show(gameScreen.stage);
+    }
+    
+    private static void showMessagePanda1() {
+        var pop = createTable(() -> {
+            deltaMultiplier = 1;
+        });
+        
+        var image = new Image(skin.getDrawable("icon-dq"));
+        image.setScaling(Scaling.none);
+        pop.add(image);
+        
+        var label = new TypingLabel("Ooof, not another boss... Ready yourself, mortal!", skin);
+        label.setWrap(true);
+        pop.add(label).grow();
+        
+        pop.show(gameScreen.stage);
+    }
+    
+    private static void showMessageLyze1() {
+        var pop = createTable(() -> {
+            deltaMultiplier = 1;
+        });
+        
+        var image = new Image(skin.getDrawable("icon-dq"));
+        image.setScaling(Scaling.none);
+        pop.add(image);
+        
+        var label = new TypingLabel("Let's just get this over with, feathered one.", skin);
+        label.setWrap(true);
+        pop.add(label).grow();
+        
+        pop.show(gameScreen.stage);
+    }
+    
+    private static void showMessageJohn1() {
+        var pop = createTable(() -> {
+            deltaMultiplier = 1;
+        });
+        
+        var image = new Image(skin.getDrawable("icon-dq"));
+        image.setScaling(Scaling.none);
+        pop.add(image);
+        
+        var label = new TypingLabel("OMG, John! What did they do to you? I shall... release you.", skin);
         label.setWrap(true);
         pop.add(label).grow();
         
